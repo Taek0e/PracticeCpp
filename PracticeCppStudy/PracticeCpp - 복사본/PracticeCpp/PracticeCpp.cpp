9f7a94b90a1ad6eb77a3a306bcd4c9284fd48551
Taek0e <bnskim05@gmail.com>
2022-02-18 18:19:51 +0900

    2022.02.18 Cpp 공부함요~

---
+++ PracticeCppStudy/PracticeCpp - 복사본/PracticeCpp/PracticeCpp.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+
+int main(void) {
+	int num1 = 1000;
+	int &num2 = num1;
+	int& num3 = num2;
+
+	num3 = 999;
+
+	cout << num2 << " " << num1;
+	return 0;
+}
+
