3ba3e656f1d93c0a6efb6e30ca1b72aaeb0d1b53
Taek0e <bnskim05@gmail.com>
2022-02-19 17:30:46 +0900

    2022.02.19 참조자 공부 c++

--- PracticeCppStudy/PracticeCpp - 복사본/PracticeCpp/PracticeCpp.cpp
+++ PracticeCppStudy/PracticeCpp - 복사본/PracticeCpp/PracticeCpp.cpp
@@ -1,15 +1,21 @@
 ﻿#include <iostream>
 using namespace std;
 
+int& RefRetFuncOne(int& ref) {
+	ref++;
+	return ref;
+}
+
 
 int main(void) {
-	int num1 = 1000;
-	int &num2 = num1;
-	int& num3 = num2;
+	int num1 = 1;
+	int& num2 = RefRetFuncOne(num1);
 
-	num3 = 999;
-
-	cout << num2 << " " << num1;
+	num1++;
+	num2++;
+	cout << "num1 : " << num1 << endl;
+	cout << "num2 : " << num2 << endl;
+	
 	return 0;
 }
 
